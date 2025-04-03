b371af2eabddbbe2a5ec4ba2f60f965477fe4dfb
sergey.tismenko <sergey.tismenko@juliusworks.com>
2025-04-03 18:13:43 +0200

    136

---
+++ js/136.js
@@ -0,0 +1,54 @@
+/**
+ * 136. Single Number
+ * Solved
+ * Easy
+ * Topics
+ * Companies
+ * Hint
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ *
+ * You must implement a solution with a linear runtime complexity and use only constant extra space.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,2,1]
+ *
+ * Output: 1
+ *
+ * Example 2:
+ *
+ * Input: nums = [4,1,2,1,2]
+ *
+ * Output: 4
+ *
+ * Example 3:
+ *
+ * Input: nums = [1]
+ *
+ * Output: 1
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 3 * 104
+ * -3 * 104 <= nums[i] <= 3 * 104
+ * Each element in the array appears twice except for one element which appears only once.
+ *
+ *
+ * @param {number[]} nums
+ * @return {number}
+ *
+ */
+const singleNumber = function(nums) {
+    for(let i = 0; i < nums.length; i++) {
+        // console.log(nums[i])
+        if(nums.findIndex((el, index) => el === nums[i] && index !== i) !== -1) {
+            continue
+        } else{
+            return nums[i]
+        }
+    }
+}
